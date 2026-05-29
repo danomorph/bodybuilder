8fcf92c5e5c193606dac4e64d280af4200bd8192
Dan <68190171+danomorph@users.noreply.github.com>
2026-05-29 17:16:25 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bodybuilder-v1';
+const CACHE = 'bodybuilder-v1.01';
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
